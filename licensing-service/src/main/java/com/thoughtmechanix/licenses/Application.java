1a585aaeac23cc2ecca6fd2bb26a9cfe110d37d7
RAMAZANA-JVIMH3\eayoz <ramazanayoz>
2022-06-24 13:04:24 +0300

    chptr-4- initial

---
+++ licensing-service/src/main/java/com/thoughtmechanix/licenses/Application.java
@@ -0,0 +1,21 @@
+package com.thoughtmechanix.licenses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+import java.util.Collections;
+import java.util.List;
+
+@SpringBootApplication
+
+public class Application {
+
+
+  public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+  }
+}
